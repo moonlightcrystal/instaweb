5c6d0ae74911e759268224b3781a80d427aaed87
moonlightcrystal <weloveyoucris7@gmail.com>
2020-01-14 23:25:19 +0300

    frintphotofix

--- index.php
+++ index.php
@@ -44,316 +44,47 @@ session_start();
 </header>
 
 <ul class="flex-container">
-    <li class="flex-item">1</li>
-    <li class="flex-item">2</li>
-    <li class="flex-item">3</li>
-    <li class="flex-item">4</li>
-    <li class="flex-item">5</li>
-    <li class="flex-item">6</li>
+    <li>
+        <div id="picture">
+            <div id="loginPicture">
+                <img src="avatar.jpg">
+                <a href="">alfur</a>
+            </div>
+            <img src="keeet.png">
+            <div id="comment">
+                <input name="comments" type="text" placeholder="add your comment">
+                <input name="plus" type="button" value="➕">
+            </div>
+        </div>
+    </li>
+    <li>
+        <div id="picture">
+            <div id="loginPicture">
+                <img src="avatar.jpg">
+                <a href="">alfur</a>
+            </div>
+            <img src="keeet.png">
+            <div id="comment">
+                <input name="comments" type="text" placeholder="add your comment">
+                <input name="plus" type="button" value="➕">
+            </div>
+        </div>
+    </li>
+    <li>
+        <div id="picture">
+            <div id="loginPicture">
+                <img src="avatar.jpg">
+                <a href="">alfur</a>
+            </div>
+            <img src="keeet.png">
+            <div id="comment">
+                <input name="comments" type="text" placeholder="add your comment">
+                <input name="plus" type="button" value="➕">
+            </div>
+        </div>
+    </li>
+    
 </ul>
 
-<ul class="flex-container">
-    <!--    <h1 class="marquee"><span>This is a marquee. Text, text, text...</span></h1>-->
-    <li class="flex-item">
-        <div id="picture">
-            <div id="loginPicture">
-                <img src="avatar.jpg">
-                <a href="">alfur</a>
-            </div>
-            <img src="keeet.png">
-            <div id="comment">
-                <input name="comments" type="text" placeholder="add your comment">
-                <input name="plus" type="button" value="➕">
-            </div>
-        </div>
-    </li>
-    <li class="flex-item">
-        <div id="picture">
-            <div id="loginPicture">
-                <img src="avatar.jpg">
-                <a href="">alfur</a>
-            </div>
-            <img src="keeet.png">
-            <div id="comment">
-                <input name="comments" type="text" placeholder="add your comment">
-                <input name="plus" type="button" value="➕">
-            </div>
-        </div>
-    </li>
-    <li class="flex-item">
-        <div id="picture">
-            <div id="loginPicture">
-                <img src="avatar.jpg">
-                <a href="">alfur</a>
-            </div>
-            <img src="keeet.png">
-            <div id="comment">
-                <input name="comments" type="text" placeholder="add your comment">
-                <input name="plus" type="button" value="➕">
-            </div>
-        </div>
-    </li>
-    <li class="flex-item">
-        <div id="picture">
-            <div id="loginPicture">
-                <img src="avatar.jpg">
-                <a href="">alfur</a>
-            </div>
-            <img src="keeet.png">
-            <div id="comment">
-                <input name="comments" type="text" placeholder="add your comment">
-                <input name="plus" type="button" value="➕">
-            </div>
-        </div>
-    </li>
-<!--    <li id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="maxresdefault.jpg">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </li>-->
-<!--    <li id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="keeet.png">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </li>-->
-<!--    <li id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="maxresdefault.jpg">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </li>-->
-<!--    <li id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="keeet.png">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </li>-->
-<!--    <li id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="maxresdefault.jpg">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </li>-->
-</ul>
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="keeet.png">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="maxresdefault.jpg">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="keeet.png">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="maxresdefault.jpg">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="keeet.png">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="maxresdefault.jpg">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="keeet.png">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="maxresdefault.jpg">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="keeet.png">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="maxresdefault.jpg">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="keeet.png">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="maxresdefault.jpg">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="keeet.png">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <div id="Block">-->
-<!--        <div id="picture">-->
-<!--            <div id="loginPicture">-->
-<!--                <img src="avatar.jpg">-->
-<!--                <a href="">alfur</a>-->
-<!--            </div>-->
-<!--            <img src="maxresdefault.jpg">-->
-<!--            <div id="comment">-->
-<!--                <input name="comments" type="text" placeholder="add your comment">-->
-<!--                <input name="plus" type="button" value="➕">-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-</ul>
 </body>
 </html>
