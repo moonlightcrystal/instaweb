f00bb2b84aa64d499362bd1d8d967fef0dd44e19
moonlightcrystal <weloveyoucris7@gmail.com>
2020-01-27 00:00:02 +0300

    fix erors with add post

--- model/addpost.php
+++ model/addpost.php
@@ -5,40 +5,39 @@ require_once 'connection.php';
 setlocale(LC_ALL, 'ru_RU.UTF-8');
 date_default_timezone_set( 'Europe/Moscow' );
 
-function uploadImg($image)
-{
-    $extension = pathinfo($image['name'], PATHINFO_EXTENSION);
-    $filename = uniqid() . "." . $extension;
-
-    move_uploaded_file($image['tmp_name'], '../uploads/' . $filename);
-
-    return $filename;
-}
-
 function addPost($filename, $login, $title, $dbh)
 {
-    $sql =
-        "INSERT INTO `images` (`name`, `date`, `login`, `title`) VALUES (
+
+        $sql =
+            "INSERT INTO `images` (`name`, `date`, `login`, `title`) VALUES (
     :name,
     FROM_UNIXTIME(:date),
     (SELECT `login` from `users` WHERE `users`.`login` = :login),
     :title)";
-    $statement = $dbh->prepare($sql);
-    $statement->bindParam(":title", $title);
-    $statement->bindParam(":date", time());
-    $statement->bindParam(":name", $filename);
-    $statement->bindParam(":login", $login);
-    $statement->execute();
+        $statement = $dbh->prepare($sql);
+        $statement->bindParam(":title", $title);
+        $statement->bindParam(":date", time());
+        $statement->bindParam(":name", $filename);
+        $statement->bindParam(":login", $login);
+        $statement->execute();
+
 }
 
-
-function showPost($dbh)
+function register($login, $email, $password, $dbh)
 {
-    $sql = "SELECT * FROM `images`";
-    $statement = $dbh->prepare($sql);
-    $statement->execute();
-    $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+    if (!empty($login) && !empty($email) && !empty($password)) {
 
-    return $posts;
+        $password = password_hash($password, PASSWORD_DEFAULT);
+
+        try {
+            $statement = $dbh->prepare('INSERT INTO users(login, email, password) VALUES(:login, :email, :passwd)');
+            $statement->bindParam(':login', $login, PDO::PARAM_STR);
+            $statement->bindParam(':email', $email, PDO::PARAM_STR);
+            $statement->bindParam(':passwd', $password, PDO::PARAM_STR);
+            $statement->execute();
+            header('Location: ../view/signin.php');
+        } catch (PDOException $e) {
+            header('Location: ../view/signup.php');
+        }
+    }
 }
-
--- model/reg.php
+++ model/reg.php
@@ -12,12 +12,14 @@ if (!empty($login) && !empty($email) && !empty($password)) {
 
     try {
         $sth1 = $dbh->prepare('INSERT INTO users(login, email, password) VALUES(:login, :email, :passwd)');
-        $sth1->bindParam(':login', $login, PDO::PARAM_STR);
-        $sth1->bindParam(':email', $email, PDO::PARAM_STR);
-        $sth1->bindParam(':passwd', $password, PDO::PARAM_STR);
+        $sth1->bindParam(':login', $login);
+        $sth1->bindParam(':email', $email);
+        $sth1->bindParam(':passwd', $password);
         $sth1->execute();
         header('Location: ../view/signin.php');
     } catch (PDOException $e) {
         header('Location: ../view/signup.php');
     }
 }
+
+
